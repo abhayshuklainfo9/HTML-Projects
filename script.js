2476aea0eafb078e5e4f718dfbd5ad31a35ff91a
abhayshuklainfo9 <79213779+abhayshuklainfo9@users.noreply.github.com>
2022-05-29 22:59:01 +0530

    created js file for counter

---
+++ script.js
@@ -0,0 +1,10 @@
+var data=0;
+document.getElementById("num").innerText=data;
+function decrement(){
+    data=data-1;
+    document.getElementById("num").innerText=data;
+}
+function increment(){
+    data=data+1;
+    document.getElementById("num").innerText=data;
+}
